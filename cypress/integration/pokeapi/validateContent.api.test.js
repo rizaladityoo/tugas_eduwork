094435b7abcc65dd41d4664498d74b239d65560a
unknown <rizaladityoo@yahoo.com>
2023-04-26 14:50:38 +0700

    validate content

---
+++ cypress/integration/pokeapi/validateContent.api.test.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress"/>
+describe('Successfully validate content-type', () => {
+    it.only('Validate Header', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
+        cy.get('@pokemon').its('headers').its('content-type')
+            .should('include', 'application/json; charset=utf-8')
+        cy.get('@pokemon').should((response) => {
+            expect(response.body.abilities[1].ability.name).to.eq('imposter')
+        })
+
+    });
+});
